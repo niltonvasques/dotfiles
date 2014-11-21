04ae33ad081d6be2eaa5eee85fb9ae0856043617
Nilton Vasques <nilton.vasques@gmail.com>
2014-11-21 06:56:50 -0300

    Added a ask question before replace dotfiles

--- install.sh
+++ install.sh
@@ -1,16 +1,39 @@
+
+confirm () {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case $response in
+        [yY][eE][sS]|[yY]) 
+           true 
+            ;;
+        *)
+           false 
+            ;;
+    esac
+}
+
 current_dir=$(pwd)
 
 echo $current_dir
 
 for i in .*; do 
-	if [ $i != '.git' ] &&  [ $i != '.' ] && [ $i != '..' ] && [[ $i != *.swp ]]
+	if [ $i != '.git' ] &&  [ $i != '.' ] \
+		&& [ $i != '..' ] && [ $i != '.gitignore' ] \
+		&& [ $i != '.gitmodules' ] && [[ $i != *.swp ]]
 
 	then
 		target=$current_dir/$i
-		echo 'removing '~/$i;  
-		rm ~/$i -f;
-		echo 'creating symlink '~/$i ' -> '$target;  
-		ln -s $target ~/$i;
+		file=$HOME"/"$i
+		echo $file "will be removed"
+		if ( confirm )
+		then
+			echo 'removing '$file;  
+			rm $file -Rf;
+			echo 'creating symlink '$file ' -> '$target;  
+			ln -s $target $file;
+		fi
 	fi
 done
 echo 'install dotfiles finished';  
+
+
