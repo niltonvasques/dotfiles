819c568bfe28c3659eafb4640da588fc2e2a2e18
Nilton Vasques <nilton.vasques@gmail.com>
2015-03-12 10:18:18 -0300

    Improve mail sync

--- .mutt/mail-sync.sh
+++ .mutt/mail-sync.sh
@@ -5,4 +5,4 @@ while pkill --signal 0 offlineimap
 do
     sleep 2
 done
-nohup offlineimap a niltonvasques >> ~/.mutt/log/offlineimap.log 2>> ~/.mutt/log/log/offlineimap.log &
+nohup offlineimap a niltonvasques >> ~/.mutt/log/offlineimap.log 2>> ~/.mutt/log/offlineimap.log &
--- .mutt/mutt-kaefer-sync.sh
+++ .mutt/mutt-kaefer-sync.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # This script was copied from http://bkanuka.com/articles/offlineimap-mutt/
+nohup davmail /etc/davmail.properties > ~/.mutt/log/davmail-mailkaefer-log 2>> ~/.mutt/log/davmail-mailkaefer-log &
+DAVMAIL_PID=$!
+
+sleep 5
 while true      # run forever
 do
     nohup offlineimap -a niltonkaefer > ~/.mutt/log/offlineimapkaefer.log 2> /dev/null  # run offlineimap and copy log to ~/mail-log
@@ -10,6 +14,9 @@ mutt -F ~/.mutt/muttkaeferrc           # run mutt in foreground (and waits for m
 
 kill $LOOP_PID              # these two lines are a cool trick to kill the
 wait $LOOP_PID 2>/dev/null  # infinite loop and hide the error that it generates
+
+kill $DAVMAIL_PID
+wait $DAVMAIL_PID 2> /dev/null
 
 ~/.mutt/kaefer-sync.sh &     # sync mail once more after mutt exits
 exit 0          # force script to exit "cleanly")
