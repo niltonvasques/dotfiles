e347c191c62df6820fbd859718eeae12b9c1c041
Nilton Vasques <nilton.vasques@gmail.com>
2015-03-31 06:52:24 -0300

    Try call self as superuser

--- install.rb
+++ install.rb
@@ -1,11 +1,17 @@
 #!/usr/bin/env ruby
 
-def are_you_sure
-  puts "Are you sure?"
-  if gets in {'y', 'Y'}
+def are_you_sure?
+  print "Are you sure? [y/N]: "
+  if %w[y Y].include?(gets.chop)
     true
   end
   false
 end
 
-are_you_sure
+if ARGV[0] == "--superuser"
+  exit
+end
+
+if are_you_sure?
+  exec "sudo ./install.rb --superuser"
+end
