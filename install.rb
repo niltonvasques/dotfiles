7e13b9082649117a8b376754373a7422a03e4994
Nilton Vasques <nilton.vasques@gmail.com>
2015-04-02 08:04:09 -0300

    Create symlinks in ruby script

--- install.rb
+++ install.rb
@@ -8,10 +8,18 @@ def are_you_sure?
   false
 end
 
-if ARGV[0] == "--superuser"
-  exit
-end
+HOME = ENV['HOME'] 
 
-if are_you_sure?
-  exec "sudo ./install.rb --superuser"
+dotfiles = Dir['.*'] - ['.', '..']
+dotfiles -= dotfiles.select{ |dot| dot[/.git/] || dot[/.swp/] }
+dotfiles.each do |dot|
+  dest_file = "#{HOME}/#{dot}" 
+  if File.file?(dest_file) || File.directory?(dest_file)
+    puts "#{dest_file} exists and will be erased."
+    if are_you_sure?
+      system "rm -Rf #{dest_file}"
+      #system "ln -s #{dot}"
+    end
+    File.symlink(dot, dest_file)
+  end
 end
