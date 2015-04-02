7ac25659f3bfae7dc4951d7849b36b0a3e75dd75
Nilton Vasques <nilton.vasques@gmail.com>
2015-04-02 11:33:17 +0000

    Fix symlink copy when files doesn't exists

--- install.rb
+++ install.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+puts "Setup dotfiles"
 
 def are_you_sure?
   print "Are you sure? [y/N]: "
@@ -9,9 +10,12 @@ def are_you_sure?
 end
 
 HOME = ENV['HOME'] 
+puts HOME
 
 dotfiles = Dir['.*'] - ['.', '..']
+p dotfiles
 dotfiles -= dotfiles.select{ |dot| dot[/.git/] || dot[/.swp/] }
+p dotfiles
 dotfiles.each do |dot|
   dest_file = "#{HOME}/#{dot}" 
   if File.file?(dest_file) || File.directory?(dest_file)
@@ -20,6 +24,7 @@ dotfiles.each do |dot|
       system "rm -Rf #{dest_file}"
       #system "ln -s #{dot}"
     end
-    File.symlink(dot, dest_file)
   end
+  puts "Create symlink #{dest_file} -> #{dot}"
+  File.symlink(dot, dest_file)
 end
