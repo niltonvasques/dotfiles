518ac15da13a236482537e3c72d9e754c82d4f66
Nilton Vasques <nilton.vasques@gmail.com>
2015-04-03 11:23:09 -0300

    Add brazilian playlist for cmus

---
+++ .cmus/brazilian.pl
@@ -0,0 +1,187 @@
+/home/niltonvasques/Music/Lenine - Chão/01 Chão.mp3
+/home/niltonvasques/Music/Lenine - Chão/02 Se não for amor, eu cegue.mp3
+/home/niltonvasques/Music/Lenine - Chão/03 Amor é pra quem ama.mp3
+/home/niltonvasques/Music/Lenine - Chão/04 Seres estranhos.mp3
+/home/niltonvasques/Music/Lenine - Chão/05 Uma canção e só.mp3
+/home/niltonvasques/Music/Lenine - Chão/06 Envergo mas não quebro.mp3
+/home/niltonvasques/Music/Lenine - Chão/07 Malvadeza.mp3
+/home/niltonvasques/Music/Lenine - Chão/08 Tudo que me falta, nada que me sobra.mp3
+/home/niltonvasques/Music/Lenine - Chão/09 De onde vem a canção.mp3
+/home/niltonvasques/Music/Lenine - Chão/10 Isso é só o começo.mp3
+/home/niltonvasques/Music/Lenine - Banda MTV/01 Lenine - Ecos do Ão.mp3
+/home/niltonvasques/Music/Lenine - Banda MTV/02 Lenine - Rosebud (o verbo e a verba).mp3
+/home/niltonvasques/Music/Lenine - Banda MTV/03 Lenine -Caribantu.mp3
+/home/niltonvasques/Music/Lenine - Banda MTV/04 Lenine - Sonhei.mp3
+/home/niltonvasques/Music/Lenine - Banda MTV/05 Lenine - O Homem Dos Olhos De Raio X.mp3
+/home/niltonvasques/Music/Lenine - Banda MTV/06 Lenine - Lavadeira doRrio.mp3
+/home/niltonvasques/Music/Lenine - Banda MTV/07 Lenine - Encantamento.mp3
+/home/niltonvasques/Music/Lenine - Banda MTV/08 Lenine - Hoje eu Quero Sair Só.mp3
+/home/niltonvasques/Music/Lenine - Banda MTV/09 Lenine - Umbigo.mp3
+/home/niltonvasques/Music/Lenine - Labiata/01 Martelo Bigorna.mp3
+/home/niltonvasques/Music/Lenine - Labiata/02 Magra.mp3
+/home/niltonvasques/Music/Lenine - Labiata/03 Samba e Leveza.mp3
+/home/niltonvasques/Music/Lenine - Labiata/04 A Mancha.mp3
+/home/niltonvasques/Music/Lenine - Labiata/05 Lá Vem a Cidade.mp3
+/home/niltonvasques/Music/Lenine - Labiata/06 O Céu É Muito.mp3
+/home/niltonvasques/Music/Lenine - Labiata/07 É Fogo.mp3
+/home/niltonvasques/Music/Lenine - Labiata/08 É o que Me Interessa.mp3
+/home/niltonvasques/Music/Lenine - Labiata/09 Ciranda Praieira.mp3
+/home/niltonvasques/Music/Lenine - Labiata/10 Excesso Exceto.mp3
+/home/niltonvasques/Music/Lenine - Labiata/11 Continuação.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/01 Tenha dó.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/02 Descoberta.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/03 Anna Júlia.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/04 Quem sabe.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/05 Pierrot.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/06 Azedume.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/07 Lágrimas Sofridas.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/08 Primavera.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/09 Vai embora.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/10 Sem Ter Você.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/11 Onze Dias.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/12 Aline.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/13 Outro Alguém.mp3
+/home/niltonvasques/Music/Los Hermanos/[1999] Los Hermanos/14 Bárbara.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/01 - Todo Carnaval Tem Seu Fim.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/02 - A Flor.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/03 - Retrato Pra Iaiá.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/04 - Assim Será.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/05 - Casa Pré-Fabricada.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/06 - Cadê Teu Suín-.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/07 - Sentimental.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/08 - Cher Antoine.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/09 - Deixa Estar.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/10 - Mais Uma Canção.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/11 - Fingi Na Hora Rir.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/12 - Veja Bem Meu Bem.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/13 - Tão Sozinho.mp3
+/home/niltonvasques/Music/Los Hermanos/[2001] Bloco Do Eu Sozinho/14 - Adeus Você.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/01. Samba A Dois.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/02. O Vencedor.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/03. Tá Bom.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/04. Último Romance.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/05. Do Sétimo Andar.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/06. A Outra.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/07. Cara Estranho.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/08. O Velho E O Moço.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/09. Além Do Que Se Vê.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/10. O Pouco Que Sobrou.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/11. Conversa De Botas Batidas.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/12. Deixa O Verão.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/13. Do Lado De Dentro.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/14. Um Par.mp3
+/home/niltonvasques/Music/Los Hermanos/[2003] Ventura/15. De Onde Vem A Calma.mp3
+/home/niltonvasques/Music/Los Hermanos/[2005] 4/01 - Dois Barcos.mp3
+/home/niltonvasques/Music/Los Hermanos/[2005] 4/02 - Primeiro Andar.mp3
+/home/niltonvasques/Music/Los Hermanos/[2005] 4/03 - Fez-se Mar.mp3
+/home/niltonvasques/Music/Los Hermanos/[2005] 4/04 - Paquetá.mp3
+/home/niltonvasques/Music/Los Hermanos/[2005] 4/05 - Os Pássaros.mp3
+/home/niltonvasques/Music/Los Hermanos/[2005] 4/06 - Morena.mp3
+/home/niltonvasques/Music/Los Hermanos/[2005] 4/07 - O Vento.mp3
+/home/niltonvasques/Music/Los Hermanos/[2005] 4/08 - Horizonte Distante.mp3
+/home/niltonvasques/Music/Los Hermanos/[2005] 4/09 - Condicional.mp3
+/home/niltonvasques/Music/Los Hermanos/[2005] 4/10 - Sapato Novo.mp3
+/home/niltonvasques/Music/Los Hermanos/[2005] 4/11 - Pois é.mp3
+/home/niltonvasques/Music/Los Hermanos/[2005] 4/12 - É de lágrima.mp3
+/home/niltonvasques/Music/Raul Seixas/1976 - HÁ 10 MIL ANOS ATRÁS/01.Canto Para Minha Morte.mp3
+/home/niltonvasques/Music/Raul Seixas/1976 - HÁ 10 MIL ANOS ATRÁS/02.Meu Amigo Pedro.mp3
+/home/niltonvasques/Music/Raul Seixas/1976 - HÁ 10 MIL ANOS ATRÁS/03.Ave Maria da Rua.mp3
+/home/niltonvasques/Music/Raul Seixas/1976 - HÁ 10 MIL ANOS ATRÁS/04.Quando Você Crescer.mp3
+/home/niltonvasques/Music/Raul Seixas/1976 - HÁ 10 MIL ANOS ATRÁS/05.O Dia da Saudade.mp3
+/home/niltonvasques/Music/Raul Seixas/1976 - HÁ 10 MIL ANOS ATRÁS/06.Eu Também Vou Reclamar.mp3
+/home/niltonvasques/Music/Raul Seixas/1976 - HÁ 10 MIL ANOS ATRÁS/07.As Minas do Rei Salomão.mp3
+/home/niltonvasques/Music/Raul Seixas/1976 - HÁ 10 MIL ANOS ATRÁS/09.O Homem.mp3
+/home/niltonvasques/Music/Raul Seixas/1976 - HÁ 10 MIL ANOS ATRÁS/10.Os Números.mp3
+/home/niltonvasques/Music/Raul Seixas/1976 - HÁ 10 MIL ANOS ATRÁS/11.Cantiga de Ninar.mp3
+/home/niltonvasques/Music/Raul Seixas/1976 - HÁ 10 MIL ANOS ATRÁS/12.Eu Nasci há Dez Mil Anos Atrás.mp3
+/home/niltonvasques/Music/RAUL SEIXAS/Raul Seixas - 02 - Meu Amigo Pedro - CD - HÃ¡ Dez Mil Anos AtrÃ¡s.mp3
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/01 - Ouro de tolo.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/02 - Sociedade alternativa.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/03 - Metamorfose ambulante.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/04 - Tente outra vez.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/05 - Fim de m s.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/06 - Gita.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/07 - Al capone.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/08 - o trem das sete.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/09 - novo aeon.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/10 - rock do diabo.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/11 - a ma.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/12 - mosca na sopa.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/13 - eu tamb m vou reclamar.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/14 - dentadura posti a.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/15 - a hora do trem passar.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/16 - o homem.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/17 - os n meros.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/18 - eu nasci h  dez mil anos atr s.ogg
+/home/niltonvasques/Music/RAUL SEIXAS/Francisco_CarriÃ§o_-_Eu_Nasci_Ha_Dez_Mil_Anos_Atras_-_Raul_Seixas.mp3
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/19 - medo da chuva.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/20 - para n ia.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/21 - sess o das dez.ogg
+/home/niltonvasques/Music/Raul Seixas/1982 - A Arte de Raul Seixas/22 - superher is.ogg
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/01 A Dança das Borboletas.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/02 Táxi Lunar.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/03 Banquete de Signos.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/04 Canção Agalopada.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/05 Eternas Ondas.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/06 Avôhai.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/07 Vila do Sucesso.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/08 Chão de Giz.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/09 Garoto de Aluguel.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/10 Admirável Gado Novo.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/11 Batendo na Porta do Céu.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/12 Sinônimos.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/13 Entre a Serpente e a Estrela.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/14 Mistérios da Meia Noite.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/15 Frevo Mulher.wma
+/home/niltonvasques/Music/Zé Ramalho - Ao Vivo/16 Corações Animais.wma
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/01 - Detonautas - o retorno de saturno.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/02 - Detonautas - Sonhos Verdes.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/03 - Detonautas - O Dia que nao terminou.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/04 - Detonautas - O Amanha.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/05 - Detonautas - Nem me lembro mais.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/06 - Detonautas - Olhos Certos.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/07 - Detonautas - Dia Comum.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/08 - Detonautas - Ate quando esperar.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/09 - Detonautas - Quando o sol se for.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/10 - Detonautas - Inferno sao os outros.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/11 - Detonautas - So nos 2.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/12 - Detonautas - So por Hoje - Tenis Rock.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/13 - Detonautas - Outro Lugar.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas -Acustico/14 - Detonautas - Voce me faz tao bem.mp3
+/home/niltonvasques/Music/Detonautas/Detonautas - O dia que não terminou.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/01 - O Papa é Pop.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/02 - Até o Fim.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/03 - Vida Real.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/04 - Infinita Highway.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/05 - Armas Químicas e Poemas.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/06 - O Preço.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/07 - Dom Quixote.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/08 - 3x4.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/09 - Refrão de Bolero.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/10 - Surfando em Karmas & DNA.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/11 - Depois de Nós.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/12 - 3ª do Plural.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/13 - Terra de Gigantes -  Números.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/14 - Somos Quem Podemos Ser.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/15 - Outras Freqüências.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/16 - Pose.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/17 - A Revolta dos Dândis.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2004] Acustico MTV/18 - Era um Garoto....mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/01 - Toda Forma De Poder + Chuva De Containers.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/02 - Vertical.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/03 - Guantánamo.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/04 - A Montanha.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/05 - Quebra-Cabeça.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/06 - No Meio De Tudo Você.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/07 - Não Consigo Odiar Ninguém.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/08 - Cinza.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/09 - Coração Blindado.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/10 - A Onda.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/11 - Parabólica.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/12 - Faz De Conta.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/13 - Novos Horizontes.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/14 - Alívio Imediato.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/15 - Simples De Coração.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/16 - Piano Bar.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/17 - Luz.mp3
+/home/niltonvasques/Music/Engenheiros do Hawaii/[2007] Novos Horizontes Acustico/18 - Pra Ser Sincero.mp3
