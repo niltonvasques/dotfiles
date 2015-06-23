fab5d277800046268af8fb1a6c7aefcc552092ed
Nilton Vasques <nilton.vasques@gmail.com>
2015-06-23 12:41:29 -0300

    Add external ip address to tmux powerline bar.
    Add gpg settings to mutt.

--- .mutt/mutt-kaefer-sync.sh
+++ .mutt/mutt-kaefer-sync.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 # This script was copied from http://bkanuka.com/articles/offlineimap-mutt/
-nohup davmail /etc/davmail.properties > ~/.mutt/log/davmail-mailkaefer-log 2>> ~/.mutt/log/davmail-mailkaefer-log &
-DAVMAIL_PID=$!
-
-sleep 5
-while true      # run forever
-do
-    nohup offlineimap -a niltonkaefer > ~/.mutt/log/offlineimapkaefer.log 2> /dev/null  # run offlineimap and copy log to ~/mail-log
-    sleep 120   # sleep 2 minutes
-done &          # run loop in background
-LOOP_PID=$!     # copy PID of loop
+#nohup davmail /etc/davmail.properties > ~/.mutt/log/davmail-mailkaefer-log 2>> ~/.mutt/log/davmail-mailkaefer-log &
+#DAVMAIL_PID=$!
+#
+#sleep 5
+#while true      # run forever
+#do
+#    nohup offlineimap -a niltonkaefer > ~/.mutt/log/offlineimapkaefer.log 2> /dev/null  # run offlineimap and copy log to ~/mail-log
+#    sleep 120   # sleep 2 minutes
+#done &          # run loop in background
+#LOOP_PID=$!     # copy PID of loop
 mutt -F ~/.mutt/muttkaeferrc           # run mutt in foreground (and waits for mutt to exit)
-
-kill $LOOP_PID              # these two lines are a cool trick to kill the
-wait $LOOP_PID 2>/dev/null  # infinite loop and hide the error that it generates
-
-kill $DAVMAIL_PID
-wait $DAVMAIL_PID 2> /dev/null
-
-~/.mutt/kaefer-sync.sh &     # sync mail once more after mutt exits
+#
+#kill $LOOP_PID              # these two lines are a cool trick to kill the
+#wait $LOOP_PID 2>/dev/null  # infinite loop and hide the error that it generates
+#
+#kill $DAVMAIL_PID
+#wait $DAVMAIL_PID 2> /dev/null
+#
+#~/.mutt/kaefer-sync.sh &     # sync mail once more after mutt exits
 exit 0          # force script to exit "cleanly")
 
--- .mutt/mutt-rip-sync.sh
+++ .mutt/mutt-rip-sync.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # This script was copied from http://bkanuka.com/articles/offlineimap-mutt/
 
-while true      # run forever
-do
-    offlineimap -a niltonrip 2> ~/.mutt/log/offlineimaprip.log  # run offlineimap and copy log to ~/mail-log
-    sleep 120   # sleep 2 minutes
-done &          # run loop in background
-LOOP_PID=$!     # copy PID of loop
+#while true      # run forever
+#do
+#    offlineimap -a niltonrip 2> ~/.mutt/log/offlineimaprip.log  # run offlineimap and copy log to ~/mail-log
+#    sleep 120   # sleep 2 minutes
+#done &          # run loop in background
+#LOOP_PID=$!     # copy PID of loop
 mutt -F ~/.mutt/muttriprc           # run mutt in foreground (and waits for mutt to exit)
 
-kill $LOOP_PID              # these two lines are a cool trick to kill the
-wait $LOOP_PID 2>/dev/null  # infinite loop and hide the error that it generates
-
-~/.mutt/rip-sync.sh &     # sync mail once more after mutt exits
+#kill $LOOP_PID              # these two lines are a cool trick to kill the
+#wait $LOOP_PID 2>/dev/null  # infinite loop and hide the error that it generates
+#
+#~/.mutt/rip-sync.sh &     # sync mail once more after mutt exits
 exit 0          # force script to exit "cleanly")
 
