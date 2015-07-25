eece9bf40dfd9de53c0eb02d1af3c66e19df46b4
Nilton Vasques <nilton.vasques@gmail.com>
2015-07-25 01:50:05 -0300

    remove unused scripts

--- .mutt/mail-sync.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# This script was copied from http://bkanuka.com/articles/offlineimap-mutt/
-
-while pkill --signal 0 offlineimap
-do
-    sleep 2
-done
-nohup offlineimap a niltonvasques >> ~/.mutt/log/offlineimap.log 2>> ~/.mutt/log/offlineimap.log &
