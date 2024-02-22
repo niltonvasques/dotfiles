891848fc8ec00ebdb279482e354f123709fb49bb
Nilton Vasques <nilton.vasques@gmail.com>
2024-02-22 00:40:39 -0300

    add fuzzy jump function

--- bash_functions.sh
+++ bash_functions.sh
@@ -269,3 +269,17 @@ fshow_preview() {
 mem() {
   ps -C "$1" -O rss | awk '{ count ++; sum += $2 }; END {count --; print "Number of processes:\t",count; print "Mem. usage per process:\t",sum/1024/count, "MB"; print "Total memory usage:\t", sum/1024, "MB" ;};';
 }
+
+# j - fuzzy jump to directory
+j() {
+    local preview_cmd="ls {2..}"
+    if command -v exa &> /dev/null; then
+        preview_cmd="exa -l {2}"
+    fi
+
+    if [[ $# -eq 0 ]]; then
+                 cd "$(autojump -s | sort -k1gr | awk -F : '$1 ~ /[0-9]/ && $2 ~ /^\s*\// {print $1 $2}' | fzf --height 40% --reverse --inline-info --preview "$preview_cmd" --preview-window down:50% | cut -d$'\t' -f2- | sed 's/^\s*//')"
+    else
+        cd $(autojump $@)
+    fi
+}
